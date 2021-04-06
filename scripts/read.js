5fe48725f4b5ff9dbfaac072f31a732b4b0a2c94
W4ff1e <72278085+W4ff1e@users.noreply.github.com>
2021-04-05 23:02:59 -0400

    Attempted tp fix read.js
    Trying to get read.js to parse an external JSON file.

--- scripts/read.js
+++ scripts/read.js
@@ -1,6 +1,8 @@
-var novelInfo = JSON.parse(JSON/reverendInsanity.json)
+
+const json = require(JSON/novels.json)
+//var novelInfo = JSON.parse(novels.json)
 function test(){
-    document.getElementById("novelTitle").innerHTML = novelInfo.ReverendInsanity.name;
+    document.getElementById("novelTitle").innerHTML = novelInfo[0].name;
     alert(novelInfo[0].name);
     console.log("I WORK");
 };
